f9d7bd6139117a7e1ac9ebe4971d76f8805110ea
Dmitriy Zuykov <dmitriy.zuykov@altarix.ru>
2022-01-23 16:10:20 +0400

    another fix

--- dj/q/admin.py
+++ dj/q/admin.py
@@ -4,18 +4,37 @@ from django.contrib import admin
 from django.db import models                                                                                                                                                                                                                                                  
 from django.forms import CheckboxSelectMultiple  
 
-from .models import Partner, Client, Quest, Order, Review, TimeSlot
+from .models import Partner, Client, Quest, Order, Review, TimeSlot, PhotoAux, Discount, City
 
+class inLinePhoto(admin.StackedInline):
+	model = PhotoAux
 
-class MyModelAdmin(admin.ModelAdmin):
+class QuestAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
+    inlines = [inLinePhoto]
+
+
+class Quest2(Quest):
+	class Meta:
+		proxy = True
+		verbose_name = 'Квест партнёра'
+		verbose_name_plural = 'Квесты партнёра'
+class QuestForPartner(QuestAdmin):
+	def get_queryset(self, request):
+		p = Partner.objects.get(user=request.user)
+		qs = super().get_queryset(request)
+		return qs.filter(partner = p)
 
 
 admin.site.register(Partner)
 admin.site.register(Client)
-admin.site.register(Quest, MyModelAdmin)
+admin.site.register(Quest2, QuestForPartner)
+admin.site.register(Quest, QuestAdmin)
 admin.site.register(Review)
 admin.site.register(Order)                                                                                                                                                                                                                                
-admin.site.register(TimeSlot)    
+admin.site.register(TimeSlot)
+admin.site.register(City)
+admin.site.register(Discount)
+#p0o9i8u7y6
--- dj/q/models.py
+++ dj/q/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
+class City(models.Model):
+	class Meta:
+		verbose_name = 'Город'
+		verbose_name_plural = 'Города'
+	def __str__(self):
+		return self.name
+	name = models.CharField(max_length=200)
+
 class Client(models.Model):
 	class Meta:
 		verbose_name = 'Клиент'
@@ -20,7 +29,7 @@ class Partner(models.Model):
 		verbose_name_plural = 'Партнёры'
 	name = models.CharField(max_length=200)
 	contact = models.CharField(max_length=200)
-
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.name
 
@@ -41,9 +50,19 @@ class Quest(models.Model):
 		return self.name
 	name = models.CharField(max_length=200)
 	price = models.CharField(max_length=200)
+	address = models.CharField(max_length=200)
+	phone = models.CharField(max_length=200)
+	socialMedia = models.CharField(max_length=200)
+	playersCount = models.CharField(max_length=200)
 	partner = models.ForeignKey(Partner, on_delete=models.CASCADE)
 	photo = models.FileField(null=True, default=None)
 	timeslot_list = models.ManyToManyField(TimeSlot)
+	city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+
+class PhotoAux(models.Model):
+	photo = models.FileField(null=True, default=None)
+	quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
 
 class Order(models.Model):
 	class Meta:
@@ -60,3 +79,9 @@ class Review(models.Model):
 		verbose_name_plural = 'Обзоры'
 	quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
 	text = models.CharField(max_length=200)
+
+class Discount(models.Model):
+	class Meta:
+		verbose_name = 'Скидка'
+		verbose_name_plural = 'Скидки'
+	quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
